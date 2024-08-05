1cc25a56742ac262b0f125ab34c8b54880b00241
SeoMGyeong <book3022@gmail.com>
2024-08-05 22:20:12 +0900

    240805 Header 아이콘 간격 완성

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,9 +9,9 @@ const Header = () => {
   const { width } = useWindowDimensions(); // 화면의 너비를 가져옵니다.
 
   return (
-    <View style={[styles.container, { width }]}>
+    <View style={styles.container}>
       <Image
-        source={require('../assets/images/react-logo.png')}
+        source={require('../assets/images/BetterIcon.jpg')}
         style={styles.logo}
         resizeMode="contain"
       />
@@ -20,13 +20,14 @@ const Header = () => {
           name="search"
           size={24}
           color="black"
-          style={styles.icon}
+          style={[styles.icon]}
           onPress={() => navigation.navigate('SearchScreen')}
         />
         <Ionicons
           name="cart"
           size={24}
           color="black"
+          style={[styles.icon, { marginLeft: 5 }]}
           onPress={() => navigation.navigate('CartScreen')}
         />
       </View>
@@ -36,18 +37,16 @@ const Header = () => {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'green',
     width: '100%',
-    justifyContent: 'center',
     flexDirection: 'row',
-    alignItems: 'center',
-    paddingHorizontal: 30, // Header의 왼쪽과 오른쪽 여백을 설정합니다.
     height: 50,
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   paddingHorizontal: 30, // Header의 왼쪽과 오른쪽 여백을 설정합니다.
   },
   logo: {
-    backgroundColor: 'white',
     width: 100,
-    height: 40,
+    height: '100%',
     marginRight: 'auto', // 로고와 아이콘 사이에 자동으로 공간을 추가하여 아이콘을 오른쪽으로 밀어냅니다.
     padding: 'auto',
   },
@@ -58,7 +57,7 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end', // 아이콘들을 오른쪽 끝으로 밀어냅니다.
   },
   icon: {
-    marginLeft: 10, // 아이콘 사이의 간격을 설정합니다.
+    marginRight: 10, // 아이콘 사이의 간격을 설정합니다.
   },
 });
 
